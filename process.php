a85edacf6242f19b302f7451aea3e1e5828e10d6
Toju Rex <rextj121@gmail.com>
2020-02-11 15:36:23 +0100

    test

--- process.php
+++ process.php
@@ -2,44 +2,38 @@
 <?php require_once("vendor/swiftmailer/swiftmailer/lib/swift_required.php"); ?>
 <?php
     if(isset($_POST['submit'])){
-        
-         $name= $_POST['name'];
-         $email= $_POST['email'];
-         $tel= $_POST['tel'];
-         $passs= $_POST['password'];
-         $messages= "
-         This is to let everyone Know that Mr. {$name} phone number is $tel and his email is $email.This is his password: $passs.
-         Thanks!
-       ";
-    }else{
+    
+        $name= $_POST['name'];
+        $email= $_POST['email'];
+        $tel= $_POST['tel'];
+        $passs= $_POST['password'];
+        $messages= "
+            This is to let everyone Know that Mr. {$name} phone number is $tel and his email is $email.This is his password: $passs.
+            Thanks!
+            ";
+        }else{
         header("Location: index.php");
     }
 
     
-    $transport = (new Swift_SmtpTransport('smtp.mailgun.org', 587, 'TLS')) 
+    $transport = (new Swift_SmtpTransport('smtp.mailgun.org', 587, 'TSL')) 
     ->setUsername('postmaster@sandbox57f54ce29a6046f3a936f12768b32f30.mailgun.org')
     ->setPassword('0a5a3ef2bc9b133d96a7d67521466908-52b6835e-7f7139ee')
     ;
-// $mailer = new Swift_Mailer($transport);
-// $message = (new Swift_Message('Wonderful Subject'))
-//     ->setFrom('rextj121@gmail.com')
-//     ->setTo('tojufutughe@gmail.com')
-//     ->setBody('Testing one two three');
-// $result = $mailer->send($message);
 
-$mailer = new Swift_Mailer($transport);
+    $mailer = new Swift_Mailer($transport);
 
-// Create a message
-$message = (new Swift_Message('Board of Directors'))
-  ->setFrom(['rextj121@gmail.com' => 'CEO'])
-  ->setTo(['tojufutughe@gmail.com' => 'Chairman'])
+    // Create a message
+    $message = (new Swift_Message('Board of Directors'))
+    ->setFrom(['rextj121@gmail.com' => 'CEO'])
+    ->setTo(['tojufutughe@gmail.com' => 'Chairman'])
 
-  ->setBody($messages)
-  ;
+    ->setBody($messages)
+    ;
 
 
 
-// Send the message
-$result = $mailer->send($message);
+    // Send the message
+    $result = $mailer->send($message);
 
 ?>
